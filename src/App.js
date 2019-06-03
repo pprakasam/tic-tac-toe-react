9c93abcbf5f789b322e76d984ee2013d8d92ed52
pprakasam <padmaprakasam@gmail.com>
2019-06-03 16:43:15 -0400

    Add code for check-winner

--- src/App.js
+++ src/App.js
@@ -5,30 +5,68 @@ class App extends Component {
   constructor () {
     super()
     this.state = {
-      turn: 'O',
-      board: Array(9).fill('')
+      turn: 'X',
+      gameEnded: false,
+      winner: ''
+    }
+    this.gameState = {
+      board: Array(9).fill(''),
+      totalMoves: 0
     }
   }
   handleClick = (event) => {
-    const boardCopy = this.state.board.slice()
-    if (!boardCopy[event.target.dataset.square]) {
-      boardCopy[event.target.dataset.square] = this.state.turn
+    if (this.state.gameEnded) return
+    if (!this.gameState.board[event.target.dataset.square]) {
+      this.gameState.board[event.target.dataset.square] = this.state.turn
       event.target.innerText = this.state.turn
-      console.log(this.state.turn)
       this.setState({
-        board: boardCopy,
         turn: this.state.turn === 'X' ? 'O' : 'X'
+      })
+      this.gameState.totalMoves++
+    }
+    if (this.checkWinner() === 'X') {
+      this.setState({
+        gameEnded: true,
+        winner: 'X',
+        message: 'Winner is X'
+      })
+    } else if (this.checkWinner() === 'O') {
+      this.setState({
+        gameEnded: true,
+        winner: 'O',
+        message: 'Winner is O'
+      })
+    } else if (this.checkWinner() === 'TIE') {
+      this.setState({
+        gameEnded: true,
+        winner: 'TIE',
+        message: 'TIE between X and O'
       })
     }
   }
 
+  checkWinner () {
+    const board = this.gameState.board
+    const winnerMoves = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7],
+      [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+    for (let i = 0; i < winnerMoves.length; i++) {
+      if (board[winnerMoves[i][0]] === board[winnerMoves[i][1]] &&
+      board[winnerMoves[i][1]] === board[winnerMoves[i][2]]) {
+        return board[winnerMoves[i][0]]
+      }
+    }
+    if (this.gameState.totalMoves === 9) {
+      return 'TIE'
+    }
+  }
+
   render () {
-    console.log(this.state.board)
     return (
       <div className="game">
         <div className="head">
           <h1>TIC TAC TOE</h1>
         </div>
+        <div className="game-status">{this.state.message}</div>
         <div className="board" onClick={this.handleClick}>
           <div className="square" data-square="0"></div>
           <div className="square" data-square="1"></div>
